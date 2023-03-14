ed81320ca16d811191702a2dab0fd491fcaef56c
Truong-Quang-Dat <100523403+Truong-Quang-Dat@users.noreply.github.com>
2023-03-15 00:13:39 +0700

    Initial commit

---
+++ src/routes/news.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const newsController = require('../app/controllers/NewsController');
+
+// newsController.index, chuyen doi tuong tu ben NewsController.js
+
+router.use('/:slug', newsController.show);
+router.use('/', newsController.index);
+
+
+module.exports = router;
